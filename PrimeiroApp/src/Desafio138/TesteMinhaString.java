3749227e3e5482265e8c275544fedb96529f0ed9
Dennis Ribeiro <dennis.ribeiro@uol.com.br>
2018-03-20 18:21:42 -0300

    Novas

--- PrimeiroApp/src/Desafio138/TesteMinhaString.java
+++ PrimeiroApp/src/Desafio138/TesteMinhaString.java
@@ -2,6 +2,7 @@ package Desafio138;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 public class TesteMinhaString {
@@ -16,12 +17,22 @@ public class TesteMinhaString {
 
 		vetStr.add(s2);
 		vetStr.add(s3);
-		vetStr.add(s1);		
+		vetStr.add(s1);
 
+		// Ordem de inclusão...
 		System.out.println(vetStr);
 
+		// Minha ordenação com o "implements ComparteTo"...
 		Collections.sort(vetStr);
-
 		System.out.println(vetStr);
+
+		// Misturando os itens da lista...
+		Collections.shuffle(vetStr);
+		System.out.println(vetStr);
+
+		Iterator<MinhaString> i = vetStr.iterator();
+		while (i.hasNext()) {			
+			System.out.println(i.next());			
+		}
 	}
 }
--- PrimeiroApp/src/Exercicio412/Conta.java
+++ PrimeiroApp/src/Exercicio412/Conta.java
@@ -98,4 +98,32 @@ public class Conta {
 
 		return info;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((agencia == null) ? 0 : agencia.hashCode());
+		result = prime * result + numero;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Conta other = (Conta) obj;
+		if (agencia == null) {
+			if (other.agencia != null)
+				return false;
+		} else if (!agencia.equals(other.agencia))
+			return false;
+		if (numero != other.numero)
+			return false;
+		return true;
+	}
 }
